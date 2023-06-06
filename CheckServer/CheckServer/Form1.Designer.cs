5907727858fac8e96421125d3a7e0b4a97050fc0
Lê Đoàn Trà My <111175444+Ramii2633@users.noreply.github.com>
2023-06-06 10:24:49 +0700

    FTP

---
+++ CheckServer/CheckServer/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CheckServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            btnCheck = new Button();
+            tbHost = new TextBox();
+            label2 = new Label();
+            cbIsIP = new CheckBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(8, 7);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(576, 375);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // btnCheck
+            // 
+            btnCheck.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnCheck.Location = new Point(466, 487);
+            btnCheck.Name = "btnCheck";
+            btnCheck.Size = new Size(112, 42);
+            btnCheck.TabIndex = 1;
+            btnCheck.Text = "Kiểm tra";
+            btnCheck.UseVisualStyleBackColor = true;
+            btnCheck.Click += btnCheck_Click;
+            // 
+            // tbHost
+            // 
+            tbHost.Location = new Point(180, 429);
+            tbHost.Name = "tbHost";
+            tbHost.Size = new Size(336, 31);
+            tbHost.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(26, 395);
+            label2.Name = "label2";
+            label2.Size = new Size(232, 25);
+            label2.TabIndex = 4;
+            label2.Text = "Thông tin máy cần kiểm tra:";
+            // 
+            // cbIsIP
+            // 
+            cbIsIP.AutoSize = true;
+            cbIsIP.Location = new Point(61, 431);
+            cbIsIP.Name = "cbIsIP";
+            cbIsIP.Size = new Size(115, 29);
+            cbIsIP.TabIndex = 5;
+            cbIsIP.Text = "Địa chỉ IP:";
+            cbIsIP.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(591, 538);
+            Controls.Add(cbIsIP);
+            Controls.Add(label2);
+            Controls.Add(tbHost);
+            Controls.Add(btnCheck);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Kiểm tra dịch vụ FTP";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Button btnCheck;
+        private TextBox tbHost;
+        private Label label2;
+        private CheckBox cbIsIP;
+    }
+}
---
+++ CheckServer/CheckServer/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+
+namespace CheckServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            // Lấy địa chỉ IP hoặc tên máy cần kiểm tra từ ô textbox
+            string host = tbHost.Text.Trim();
+
+            // Kiểm tra xem checkbox "Địa chỉ IP" được chọn hay không
+            bool isIpChecked = cbIsIP.Checked;
+
+            // Nếu checkbox được chọn, địa chỉ nhập vào được xem là địa chỉ IP
+            IPAddress ipAddress = null;
+            if (isIpChecked && !IPAddress.TryParse(host, out ipAddress))
+            {
+                MessageBox.Show("Địa chỉ nhập vào không phải là địa chỉ IP.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Nếu checkbox không được chọn, thử giải thích tên máy thành địa chỉ IP
+            if (!isIpChecked)
+            {
+                try
+                {
+                    IPHostEntry hostEntry = Dns.GetHostEntry(host);
+                    ipAddress = hostEntry.AddressList[0];
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Không thể giải thích tên máy.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+
+            // Tạo đối tượng Socket
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+           
+            try
+            {
+                // Kết nối tới cổng FTP (cổng 21) của máy
+                socket.Connect(ipAddress, 21);
+
+                if (socket.Connected)
+                {
+                    MessageBox.Show($"Dịch vụ FTP đang chạy trên máy {host} ({ipAddress}).", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show($"Dịch vụ FTP không đang chạy trên máy {host} ({ipAddress}).", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Địa chỉ IP là null.", "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (SocketException ex)
+            {
+                MessageBox.Show($"Dịch vụ FTP không đang chạy trên máy {host} ({ipAddress}).", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            finally
+            {
+                // Đóng kết nối
+                socket.Close();
+            }
+        }
+
+    }
+}
